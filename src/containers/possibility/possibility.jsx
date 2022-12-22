5662d986a6176d35d139b3b5936706a807e80754
tarikulIslamNahid <jttarekulnahid@gmail.com>
2022-12-22 15:44:15 +0600

    files and folders created

---
+++ src/containers/possibility/possibility.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './possibility.css'
+const possibility = () => {
+  return (
+    <div>possibility</div>
+  )
+}
+
+export default possibility
---
+++ src/containers/whatGTP3/whatGTP3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGTP3.css'
+const whatGTP3 = () => {
+  return (
+    <div>whatGTP3</div>
+  )
+}
+
+export default whatGTP3
